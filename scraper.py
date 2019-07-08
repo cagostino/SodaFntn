5a01c479b88d0fbc5b4a9b8bc5af95c04e69f2c4
behr-davide <davide.behr@gmail.com>
2019-07-07 21:31:35 -0400

    updated scraper based on changes in the freemidi.org source

--- scraper.py
+++ scraper.py
@@ -1,38 +1,60 @@
-from selenium import webdriver
 from bs4 import BeautifulSoup
-from urllib.request import urlopen, urlretrieve, quote
-from urllib.parse import urljoin
+from urllib.request import urlretrieve
+from multiprocessing.pool import ThreadPool
+import requests
+import os
 
-start =10000
-def download(genres):
-	"""
-	Genres is a list of genres as strings
-	"""
-	lowergenres = []
-	for genre in genres:
-		lowergenres.append(genre.lower())
-	print(lowergenres)
-	browser = webdriver.Chrome()
 
-	for i in range(start,100000):
-		try:
-			url ='https://freemidi.org/download-'+str(i)
-		
-			u = urlopen(url)
-			html = u.read().decode('utf-8')
-			soup = BeautifulSoup(html)
+global BASE
+BASE = "http://freemidi.org"
 
-			for link in soup.select('ol'):
-				for genre in lowergenres:
-					passed =False 
-					if genre in link.getText() and not passed:
-						browser.get(url)
-						button =browser.find_element_by_id('downloadmidi')
-						button.click()
-						print('pass')
-						passed= True
-						print(i)
-					print(i)
-		except:
-			continue
 
+def cook_soup(url):
+    html = requests.get(url).content
+    return BeautifulSoup(html, "html.parser")
+
+
+def get_genre_artists(genre):
+    url = BASE + "/genre-" + str(genre)
+    soup = cook_soup(url)
+    bands = soup.find_all("div", attrs={"class": "genre-band-container"})
+    print("fetching artists", "for", genre, "...")
+    return [BASE + "/" + band.div.a["href"] for band in bands]
+
+
+def get_artist_songs(url):
+    soup = cook_soup(url)
+    songs = soup.find_all("div", attrs={"itemprop": "tracks"})
+    return [
+        (song.span.a.text.strip("\n"), BASE + "/" + song.span.a["href"])
+        for song in songs
+    ]
+
+
+class Scraper:
+    fetch_pool = ThreadPool(10)
+    download_pool = ThreadPool(10)
+
+    def __init__(self, genres=[]):
+        self.genres = genres
+
+    def download(self):
+        for genre in self.genres:
+            genre_path = os.path.realpath(".") + "/midi/" + str(genre)
+        try:
+            os.makedirs(genre_path)
+        except FileExistsError:
+            pass
+        os.chdir(genre_path)
+
+        artists = get_genre_artists(genre)
+        artists_songs = self.fetch_pool.imap_unordered(get_artist_songs, artists)
+        for songs in tqdm(artists_songs):
+            self.download_pool.imap(
+                lambda x: urlretrieve(x[1], str(x[0]) + ".mid"), songs
+            )
+
+
+if __name__ == "__main__":
+    genres = ["pop"]
+    Scraper(genres).download()
